5800fdc365e9d0d665385603408b7ff1eeaec4ea
PikachuUsedSurf <nanaosafokissi@gmail.com>
2024-07-31 15:26:14 +0300

    bug fixes for deployment

--- src/components/web-ui/about.tsx
+++ src/components/web-ui/about.tsx
@@ -5,9 +5,9 @@ export default function About() {
                 <div className="grid grid-cols-2 gap-6 lg:gap-12">
                     <div className="space-y-4">
                         <h1>About Us</h1>
-                        <h2 className=" text-4xl sm:text-4xl md:text-5xl tracking-tighter font-bold">Our Farm's Story</h2>
+                        <h2 className=" text-4xl sm:text-4xl md:text-5xl tracking-tighter font-bold">Our Farm Story</h2>
                         {aboutList.map((item) => (
-                            <p className=" text-muted-foreground text-xl">{item.story}</p>
+                            <p className=" text-muted-foreground text-xl" key={item.story}>{item.story}</p>
                         ))}
                     </div>
                     <img src="/placeholder.svg"
